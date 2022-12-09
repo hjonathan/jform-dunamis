408f153801b66dd2d38d87eb39e961043378866d
Rodrigo Quelca <rodrigo@cd ..processmaker.com>
2022-12-09 12:30:11 -0400

    add pinia 2

---
+++ dunamis-editor-next/src/components/layout/SideBar.vue
@@ -0,0 +1,46 @@
+<template>
+  <component
+    v-bind:is="itemsSideBar[activeSideBar]['component']"
+    ref="component"
+  ></component>
+</template>
+
+<script lang="ts">
+import PalletePanel from '../../DynaformEditor/PalletePanel.vue';
+import PropertiesPanel from '../../PropertiesPanel/PropertiesPanelDynamic.vue';
+import SideBarI18n from '../../i18n/sideBarI18n.vue';
+import SideBarPreview from '../../DynaformPreview/sideBarDynaformPreview.vue';
+import store from '../../../store';
+
+export default {
+  name: 'SideBar',
+  components: {
+    PalletePanel,
+    PropertiesPanel,
+    SideBarI18n,
+    SideBarPreview,
+  },
+  data() {
+    return {};
+  },
+  computed: {
+    itemsSideBar: {
+      get() {
+        return store.getters['viewManager/sideBarItems'];
+      },
+      set(val: any) {
+        store.commit('viewManager/SET_SIDE_BAR_ITEMS', val);
+      },
+    },
+    activeSideBar: {
+      get() {
+        return store.getters['viewManager/sideBarActive'];
+      },
+      set(val: any) {
+        store.commit('viewManager/SET_SIDE_BAR_ACTIVE', val);
+      },
+    },
+  },
+};
+</script>
+<style></style>
---
+++ dunamis-editor-next/src/stores/viewManager/actions.ts
@@ -0,0 +1,16 @@
+const actions = {
+    setActiveActivityBar: () => {
+        console.log("test");
+    }
+    // },
+    // setAllViews: ({ commit }: any, data: any) => {
+    //     commit('SET_ALL_VIEWS', data);
+    // },
+    // setDataMainPanel: ({ commit }: any, data: any) => {
+    //     commit('SET_DATA_MAIN_PANEL', data);
+    // },
+    // setMainPanel: ({ commit }: any, data: any) => {
+    //     commit('SET_MAIN_PANEL', data);
+    // }
+};
+export default actions;

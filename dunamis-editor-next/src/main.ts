357d8a465fae4367f37b486ab97f4b865e6e8304
Rodrigo Quelca <rodrigo@cd ..processmaker.com>
2022-12-20 11:25:40 -0400

    add vuetify as plugins

--- dunamis-editor-next/src/main.ts
+++ dunamis-editor-next/src/main.ts
@@ -1,21 +1,20 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+/**
+ * main.ts
+ *
+ * Bootstraps Vuetify and other plugins then mounts the App`
+ */
+
+// Components
 import App from './App.vue'
-// Vuetify
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify';
-import * as components from 'vuetify/components';
-import * as directives from 'vuetify/directives';
 
-const vuetify = createVuetify({
-  components,
-  directives,
-});
+// Composables
+import { createApp } from 'vue'
 
+// Plugins
+import { registerPlugins } from '@/plugins'
 
-import './assets/main.css'
-const pinia = createPinia();
-const app = createApp(App);
-app.use(vuetify);
-app.use(pinia);
-app.mount('#app');
+const app = createApp(App)
+
+registerPlugins(app)
+
+app.mount('#app')
